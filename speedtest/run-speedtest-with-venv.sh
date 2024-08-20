3a1848fdbbf2136c01af6d88f1aa601ac64b5d1f
robertbyers1111 <robertbyers1111@gmail.com>
2024-08-19 23:11:50 -0400

    Point to newer location of this project; Correct location of activate script [run-speedtest-with-venv.sh]

--- speedtest/run-speedtest-with-venv.sh
+++ speedtest/run-speedtest-with-venv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DIR=/localstore/MEGA/MEGAsync/git/source/projects/speedtest
+DIR=/localstore/MEGA/MEGAsync/git/source/projects/portfolio-2024/speedtest
 BASE=speedtest-runner.py
 EXE=$DIR/$BASE
 
 cd $DIR
-source $DIR/bin/activate
+source $DIR/.venv/bin/activate
 ps -efj | grep -q "python3 .*${BASE}$"
 [ $? -ne 0 ] && {
     $EXE 2>&1 >> $DIR/console.log 2>&1
